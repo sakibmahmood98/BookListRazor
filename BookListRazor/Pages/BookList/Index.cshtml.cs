e2ff66a2c18fadaef51a02a53abf1b403618bb8e
DESKTOP-O8S8RU1\BS668 <Sakib.Mahmood@brainstation-23.com>
2022-02-02 11:27:19 +0600

    Added upsert routing & chtml

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -30,7 +30,7 @@ namespace BookListRazor.Pages.BookList
             _db.Book.Remove(book);
             await _db.SaveChangesAsync();
 
-            return RedirectToAction("Index");
+            return RedirectToPage("Index");
         }
     }
 }
